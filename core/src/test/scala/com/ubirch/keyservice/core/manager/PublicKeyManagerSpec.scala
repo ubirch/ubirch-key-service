663c244780ed939ccf4e090b77a2b9f5fc529b8d
cvandrei <crille@indarium.de>
2017-05-10 12:30:55 +0200

    UBI-63: finish create() tests

--- core/src/test/scala/com/ubirch/keyservice/core/manager/PublicKeyManagerSpec.scala
+++ core/src/test/scala/com/ubirch/keyservice/core/manager/PublicKeyManagerSpec.scala
@@ -12,7 +12,7 @@ class PublicKeyManagerSpec extends Neo4jSpec {
 
   feature("create()") {
 
-    scenario("empty database + PublicKey with all mandatory fields set") {
+    scenario("public key does not exist (PublicKey with all mandatory fields set)") {
 
       // prepare
       val publicKey = TestDataGenerator.publicKey()
@@ -29,7 +29,29 @@ class PublicKeyManagerSpec extends Neo4jSpec {
 
     }
 
-    // TODO write remaining test cases
+    scenario("public key exists (PublicKey with all mandatory fields set)") {
+
+      // prepare
+      val publicKey = TestDataGenerator.publicKey()
+      PublicKeyManager.create(publicKey) flatMap {
+
+        case None => fail(s"failed to create existing key: $publicKey")
+
+        case Some(result: PublicKey) =>
+
+          result shouldBe publicKey
+
+          // test
+          PublicKeyManager.create(publicKey) map { result =>
+
+            // verify
+            result shouldBe None
+
+          }
+
+      }
+
+    }
 
   }
 
