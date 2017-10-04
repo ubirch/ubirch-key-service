969bad6ba9e50a6541087d46c9742e6ed2d6ff7d
derMicha <dermicha@dermicha.de>
2017-10-04 16:50:33 +0200

    enhanced

--- goBuild.sh
+++ goBuild.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x
 
 SBT_CONTAINER_VERSION="latest"
-
-
+CURRENT_SERVICE_NAME="ubirch-key-service"
 
 function init() {
 
@@ -63,14 +62,14 @@ function build_container() {
 
   if [ -z $GO_PIPELINE_LABEL ]; then
       # building without GoCD
-      docker build --pull -t ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL .
+      docker build --pull -t ubirch/$CURRENT_SERVICE_NAME:v$GO_PIPELINE_LABEL .
   else
       # build with GoCD
-      docker build --pull -t ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL --build-arg GO_PIPELINE_NAME=$GO_PIPELINE_NAME \
+      docker build --pull -t ubirch/$CURRENT_SERVICE_NAME:v$GO_PIPELINE_LABEL --build-arg GO_PIPELINE_NAME=$GO_PIPELINE_NAME \
       --build-arg GO_PIPELINE_LABEL=$GO_PIPELINE_LABEL \
       --build-arg GO_PIPELINE_COUNTER=$GO_PIPELINE_COUNTER \
       --build-arg GO_STAGE_COUNTER=$GO_STAGE_COUNTER \
-      --build-arg GO_REVISION_GIT=$GO_REVISION_GIT .
+      --build-arg GO_REVISION_GIT=$GO_REVISION_SBT_GIT .
   fi
 
   if [ $? -ne 0 ]; then
@@ -79,27 +78,19 @@ function build_container() {
   fi
 
   # push Docker image
-  docker push ubirch/ubirch-key-service
-  docker push ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL
+  docker push ubirch/$CURRENT_SERVICE_NAME
+  docker push ubirch/$CURRENT_SERVICE_NAME:v$GO_PIPELINE_LABEL
   if [ $? -ne 0 ]; then
     echo "Docker push failed"
     exit 1
   fi
-
-
 }
 
 function container_tag () {
-    docker pull ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL
-    docker tag ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL ubirch/ubirch-key-service:latest
-    docker push ubirch/ubirch-key-service:latest
-
-}
-
-function container_tag_stable () {
-    docker pull ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL
-    docker tag ubirch/ubirch-key-service:v$GO_PIPELINE_LABEL ubirch/ubirch-key-service:stable
-    docker push ubirch/ubirch-key-service:stable
+    label=$1
+    docker pull ubirch/$CURRENT_SERVICE_NAME:v$GO_PIPELINE_LABEL
+    docker tag ubirch/$CURRENT_SERVICE_NAME:v$GO_PIPELINE_LABEL ubirch/$CURRENT_SERVICE_NAME:$label
+    docker push ubirch/$CURRENT_SERVICE_NAME:$label
 
 }
 
@@ -110,19 +101,18 @@ case "$1" in
         ;;
     assembly)
         build_software "clean server/assembly"
-        build_container
         ;;
     containerbuild)
         build_container
         ;;
     containertag)
-        container_tag
+        container_tag "latest"
         ;;
     containertagstable)
-        container_tag_stable
+        container_tag "stable"
         ;;
     *)
-        echo "Usage: $0 { build|assembly | containerbuild | containertag | containertagstable}"
+        echo "Usage: $0 { build | assembly | containerbuild | containertag | containertagstable }"
         exit 1
 esac
 
