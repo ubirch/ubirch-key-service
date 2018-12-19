40b3ecbded2ef3f14334adfd5fd05c2305486a4a
derMicha <dermicha@ubirch.com>
2018-12-19 21:07:31 +0100

    just a try

--- server/src/main/scala/com/ubirch/keyservice/server/Boot.scala
+++ server/src/main/scala/com/ubirch/keyservice/server/Boot.scala
@@ -2,20 +2,17 @@ package com.ubirch.keyservice.server
 
 import java.util.concurrent.TimeUnit
 
-import com.typesafe.scalalogging.slf4j.StrictLogging
-
-import com.ubirch.keyservice.config.{KeySvcConfigKeys, KeySvcConfig}
-import com.ubirch.keyservice.server.route.MainRoute
-import com.ubirch.keyservice.utils.neo4j.Neo4jSchema
-import com.ubirch.util.neo4j.utils.{Neo4jDriverUtil, Neo4jUtils}
-
-import org.neo4j.driver.v1.Driver
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.ServerBinding
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
+import com.typesafe.scalalogging.slf4j.StrictLogging
+import com.ubirch.keyservice.config.{KeySvcConfig, KeySvcConfigKeys}
+import com.ubirch.keyservice.server.route.MainRoute
+import com.ubirch.keyservice.utils.neo4j.Neo4jSchema
+import com.ubirch.util.neo4j.utils.{Neo4jDriverUtil, Neo4jUtils}
+import org.neo4j.driver.v1.Driver
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContextExecutor, Future}
@@ -27,16 +24,28 @@ import scala.language.postfixOps
   */
 object Boot extends App with StrictLogging {
 
+  logger.info("key-service will get started ...")
+
   implicit val system: ActorSystem = ActorSystem()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
   implicit val timeout: Timeout = Timeout(KeySvcConfig.timeout seconds)
 
-  implicit val neo4jDriver: Driver = Neo4jDriverUtil.driver(KeySvcConfigKeys.neo4jConfigPrefix)
+  implicit val neo4jDriver: Driver = {
+    try {
 
-  Neo4jUtils.createConstraints(Neo4jSchema.constraints)
-  Neo4jUtils.createIndices(Neo4jSchema.indices)
+      implicit val neo4jDriver: Driver = Neo4jDriverUtil.driver(KeySvcConfigKeys.neo4jConfigPrefix)
+      Neo4jUtils.createConstraints(Neo4jSchema.constraints)
+      Neo4jUtils.createIndices(Neo4jSchema.indices)
+      neo4jDriver
+    }
+    catch {
+      case e: Exception =>
+        logger.error("error while setting up neo4j connection", e)
+        throw e
+    }
+  }
 
   val bindingFuture = start()
   registerShutdownHooks()
